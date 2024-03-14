9b04feb3a723b5058a7e25b039a6916607ade061
suke0828 <mwbspa@gmail.com>
2024-03-14 18:09:01 +0900

    Add buckets
    床に設置する仕切りを作成して表示した

--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ let engine;
 let particles = [];
 let pin = [];
 let bounds = [];
+let buckets = [];
 
 function setup() {
   createCanvas(600, 700);
@@ -22,6 +23,7 @@ function setup() {
   createParticle();
   createPin();
   createBoundary();
+  createBuckets();
 }
 
 function draw() {
@@ -38,9 +40,25 @@ function draw() {
 
   // ピンの描画
   pin.map((row) => row.map((col) => col.show()));
+  // 床に設置する仕切りの描画
+  buckets.map((bucket) => bucket.show());
 
   // シミュレーションをミリ秒単位で進める(更新したいものを引数に渡す)
   Engine.update(engine, TIME_STEP);
+}
+
+function createBuckets() {
+  buckets = Array(ROWS + 1)
+    .fill(0)
+    .map((_, i) => {
+      let spacing = width / ROWS; // 隙間を与える
+      let x = floor(i * spacing);
+      let w = 10;
+      let h = 100;
+      let y = height - h / 2;
+
+      return new Boundary(x, y, w, h);
+    });
 }
 
 function createBoundary() {
