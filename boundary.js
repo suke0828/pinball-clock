f7bb5ae0625cf9aee037fb42a6d674b42426ce87
suke0828 <mwbspa@gmail.com>
2024-03-14 18:01:34 +0900

    Fixed argument

--- boundary.js
+++ boundary.js
@@ -1,9 +1,9 @@
 class Boundary {
   constructor(x, y, w, h) {
     let options = { isStatic: true };
-    this.body = Bodies.rectangle(x, y, w, h, options); // 円の物質を作成
     this.w = w;
     this.h = h;
+    this.body = Bodies.rectangle(x, y, this.w, this.h, options); // 円の物質を作成
 
     Composite.add(world, this.body); // 全ての世界にbodyを反映させる
   }
