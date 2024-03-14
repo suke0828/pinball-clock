53543e05f036f4ccc7639eff8ebfaa73c6da5cdf
suke0828 <mwbspa@gmail.com>
2024-03-14 17:59:44 +0900

    Add density

--- particle.js
+++ particle.js
@@ -3,6 +3,7 @@ class Particle {
     let options = {
       restitution: 1, // 弾性(跳ね返り)。0 ~ 1の範囲
       friction: 0.2, // 摩擦力。 0 ~ 1の範囲
+      density: 1, // 密度
     };
     this.body = Bodies.circle(x, y, r, options); // 円の物質を作成
     this.r = r; // 半径を追跡するためのプロパティ
